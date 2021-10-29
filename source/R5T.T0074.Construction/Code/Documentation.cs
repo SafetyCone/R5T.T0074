9aa0a1b059be662932926a34cc8da9bd89fd5c09
davidcoats <david@getsafetycone.com>
2021-10-29 11:07:53 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0074.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0074.Construction
+{
+    /// <summary>
+    /// Construction project for IHasConfigureApplication interface..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
